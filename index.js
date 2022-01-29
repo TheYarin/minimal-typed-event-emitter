92eab6c132e1e3bae45189149d19acb5fc3820b3
Yarin <15931306+TheYarin@users.noreply.github.com>
2022-01-29 16:29:52 +0200

    fix instantiation problems

---
+++ index.js
@@ -0,0 +1,3 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TypedEventEmitter = require("events").EventEmitter; // This is to prevent problems when trying to instantiate TypedEventEmitter.
